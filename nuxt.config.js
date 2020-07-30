4bbe7d4c77ff47a4831474604b9e9294bb556d2f
Zdeněk Jašek <jasek@sportisimo.cz>
2020-07-30 15:04:55 +0200

    PWA příprava

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,5 +17,8 @@ export default {
       // hid is used as unique identifier. Do not use `vmid` for it as it will not work
       {hid: 'description', name: 'description', content: 'Meta description'}
     ]
-  }
+  },
+  buildModules: [
+    '@nuxtjs/pwa',
+  ]
 }
